ea5d34220eca3d84ef383731695318c9714bd8ac
raziehl <matei.petroschi@gmail.com>
2023-01-19 11:39:42 +0200

    [crews-api]: First Write&Read from DB

--- crews-api/controllers/RootController.cs
+++ crews-api/controllers/RootController.cs
@@ -1,24 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
-
-namespace planets_api.Controllers;
+using crews_api.models;
+namespace crews_api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class RootController : ControllerBase
 {
   private readonly ILogger<RootController> _logger;
+  private CrewsContext db;
 
   public RootController(ILogger<RootController> logger)
   {
     _logger = logger;
-
-    _logger.LogInformation("ASD");
-    Console.WriteLine("\nASDDWWDA\n");
+    db = new CrewsContext();
   }
 
-  [HttpGet(Name = "GetWeatherForecast")]
-  public String Get()
+  [HttpGet()]
+  public CrewMember GetCrewMember()
   {
-    return "ASd";
+    try {
+      return db.CrewMembers.OrderBy(m => m.Id).FirstOrDefault();
+    } catch(Exception e) {
+      _logger.LogError(e.ToString());
+      return null;
+    }
+  }
+
+  [HttpPost]
+  [Consumes("application/json")]
+  public async Task<String> PostCrewMember(CrewMember member) {
+    try {
+      await db.CrewMembers.AddAsync(member);
+      await db.SaveChangesAsync();
+      return "Success";
+    } catch(Exception e) {
+      _logger.LogError(e.ToString());
+      return "Unsuccessful";
+    }
   }
 }
