f8262d03436b63d39c6fb994dbb38415b60ed644
raziehl <matei.petroschi@gmail.com>
2023-01-24 19:10:44 +0200

    [space-client]: logout()

--- space-client/src/app/core/auth.service.ts
+++ space-client/src/app/core/auth.service.ts
@@ -10,12 +10,21 @@ export class AuthService {
 
   constructor() {
     this.token = localStorage.getItem('JWT_TOKEN') || undefined;
+    this.user = JSON.parse(localStorage.getItem('USER_DATA') as string);
   }
 
   login(creds: { user: CrewMember, token: string }) {
     this.token = creds.token;
     localStorage.setItem('JWT_TOKEN', creds.token);
+    localStorage.setItem('USER_DATA', JSON.stringify(creds.user));
     this.user = creds.token;
+  }
+
+  logout() {
+    this.user = undefined;
+    this.token = undefined;
+    localStorage.removeItem("JWT_TOKEN");
+    localStorage.removeItem('USER_DATA');
   }
 
   isLoggedIn() {
