9b609cb948dfc6e3a938147f47e104703de78214
raziehl <matei.petroschi@gmail.com>
2023-01-25 12:49:34 +0200

    [gateway]: Initial Expeditions scaffold

---
+++ common/csharp/models/Expedition.cs
@@ -0,0 +1,25 @@
+namespace common.models;
+
+public class Expedition {
+  public int Id { get; set; }
+  public int PlanetId { get; set; }
+  public Planet Planet { get; set; }
+  public int CrewId { get; set; }
+  public Crew Crew { get; set; }
+  public List<Expedition> Expeditions { get; set; }
+  public String Status {
+    get => _status;
+    set => _status = ValidateStatus(value);
+  }
+  // public DateTime ReportDate { get; set; }
+
+
+  private string _status = PlanetStatus.TODO;
+  private String ValidateStatus(String value) {
+    if(value == PlanetStatus.OK || value == PlanetStatus.BangOK || value == PlanetStatus.TODO || value == PlanetStatus.EnRoute) {
+      return value;
+    } else {
+      return PlanetStatus.TODO;
+    }
+  }
+}
